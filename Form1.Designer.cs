6e1c5d06c9a437b3e3b557232492e06ab75aca92
Rodrigo Rodriguez <rarg_777@hotmail.es>
2019-04-29 15:43:55 -0400

    Aplicación segundo plano

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SPLANO
+{
+    partial class SEGUNDO
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.lblText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(29, 27);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(416, 20);
+            this.txtText.TabIndex = 0;
+            this.txtText.TextChanged += new System.EventHandler(this.Change);
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(26, 66);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(35, 13);
+            this.lblText.TabIndex = 1;
+            this.lblText.Text = "label1";
+            // 
+            // SEGUNDO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 102);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.txtText);
+            this.Name = "SEGUNDO";
+            this.Text = "SEGUNDO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Label lblText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SPLANO
+{
+    public partial class SEGUNDO : Form
+    {
+        public SEGUNDO()
+        {
+            InitializeComponent();
+        }
+
+        private void Change(object sender, EventArgs e)
+        {
+            lblText.Text = txtText.Text;
+        }
+    }
+}
